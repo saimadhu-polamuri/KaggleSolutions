b157b65c6e544f889be9e889122e027aec44d659
saimadhu <sai@team.quadloops.com>
2015-02-19 14:57:46 +0530

    implemented SVM approach

--- Forest_cover/forestcover.py
+++ Forest_cover/forestcover.py
@@ -1,3 +1,8 @@
+# Solution for kaggle forestcover problem 
+__autor__ = 'saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '16-Feb-2015'
+
 import pandas as pd
 import numpy as np
 import csv
@@ -9,6 +14,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 
 class ForestCover(object):
 
@@ -21,26 +27,40 @@ class ForestCover(object):
 		self.train_data = train_data
 		self.test_data = test_data
 		#print self.train_data
+	def convert_xy_traindata(self):
 
-	def randomforest_classifier_model(self):
+		""" Converts train data to x_parameters and y_parameters data """
+
+		x_train = self.train_data[['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area1','Wilderness_Area2','Wilderness_Area3','Wilderness_Area4','Soil_Type1','Soil_Type2','Soil_Type3','Soil_Type4','Soil_Type5','Soil_Type6','Soil_Type7','Soil_Type8','Soil_Type9','Soil_Type10','Soil_Type11','Soil_Type12','Soil_Type13','Soil_Type14','Soil_Type15','Soil_Type16','Soil_Type17','Soil_Type18','Soil_Type19','Soil_Type20','Soil_Type21','Soil_Type22','Soil_Type23','Soil_Type24','Soil_Type25','Soil_Type26','Soil_Type27','Soil_Type28','Soil_Type29','Soil_Type30','Soil_Type31','Soil_Type32','Soil_Type33','Soil_Type34','Soil_Type35','Soil_Type36','Soil_Type37','Soil_Type38','Soil_Type39','Soil_Type40']]
+		y_train = self.train_data['Cover_Type']
+
+		return x_train,y_train
+
+	def randomforest_classifier_model(self,x_parameters,y_parameters):
 
 		""" Fit RandomForestClassifier model with tarin data """
+
+		self.x_parameters = x_parameters
+		self.y_parameters = y_parameters
 
 		x_train = self.train_data[['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area1','Wilderness_Area2','Wilderness_Area3','Wilderness_Area4','Soil_Type1','Soil_Type2','Soil_Type3','Soil_Type4','Soil_Type5','Soil_Type6','Soil_Type7','Soil_Type8','Soil_Type9','Soil_Type10','Soil_Type11','Soil_Type12','Soil_Type13','Soil_Type14','Soil_Type15','Soil_Type16','Soil_Type17','Soil_Type18','Soil_Type19','Soil_Type20','Soil_Type21','Soil_Type22','Soil_Type23','Soil_Type24','Soil_Type25','Soil_Type26','Soil_Type27','Soil_Type28','Soil_Type29','Soil_Type30','Soil_Type31','Soil_Type32','Soil_Type33','Soil_Type34','Soil_Type35','Soil_Type36','Soil_Type37','Soil_Type38','Soil_Type39','Soil_Type40']]
 		y_train = self.train_data['Cover_Type']
 		clf = RandomForestClassifier(n_estimators=10)
-		clf = clf.fit(x_train, y_train)
+		clf = clf.fit(self.x_parameters, self.y_parameters)
 		
 		return clf 
 
-	def predict_test_data(self):
+	def predict_test_data(self,model_type):
 
 		""" predicts test data using train data randomforest classifier model """
 
-		x_test = self.test_data[['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area1','Wilderness_Area2','Wilderness_Area3','Wilderness_Area4','Soil_Type1','Soil_Type2','Soil_Type3','Soil_Type4','Soil_Type5','Soil_Type6','Soil_Type7','Soil_Type8','Soil_Type9','Soil_Type10','Soil_Type11','Soil_Type12','Soil_Type13','Soil_Type14','Soil_Type15','Soil_Type16','Soil_Type17','Soil_Type18','Soil_Type19','Soil_Type20','Soil_Type21','Soil_Type22','Soil_Type23','Soil_Type24','Soil_Type25','Soil_Type26','Soil_Type27','Soil_Type28','Soil_Type29','Soil_Type30','Soil_Type31','Soil_Type32','Soil_Type33','Soil_Type34','Soil_Type35','Soil_Type36','Soil_Type37','Soil_Type38','Soil_Type39','Soil_Type40']]
-		#x_train = self.train_data[['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area1','Wilderness_Area2','Wilderness_Area3','Wilderness_Area4','Soil_Type1','Soil_Type2','Soil_Type3','Soil_Type4','Soil_Type5','Soil_Type6','Soil_Type7','Soil_Type8','Soil_Type9','Soil_Type10','Soil_Type11','Soil_Type12','Soil_Type13','Soil_Type14','Soil_Type15','Soil_Type16','Soil_Type17','Soil_Type18','Soil_Type19','Soil_Type20','Soil_Type21','Soil_Type22','Soil_Type23','Soil_Type24','Soil_Type25','Soil_Type26','Soil_Type27','Soil_Type28','Soil_Type29','Soil_Type30','Soil_Type31','Soil_Type32','Soil_Type33','Soil_Type34','Soil_Type35','Soil_Type36','Soil_Type37','Soil_Type38','Soil_Type39','Soil_Type40']]
-		model = self.randomforest_classifier_model()
-		predicted_results = model.predict(x_test)
+		self.model_type = model_type
+
+		#x_test = self.test_data[['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area1','Wilderness_Area2','Wilderness_Area3','Wilderness_Area4','Soil_Type1','Soil_Type2','Soil_Type3','Soil_Type4','Soil_Type5','Soil_Type6','Soil_Type7','Soil_Type8','Soil_Type9','Soil_Type10','Soil_Type11','Soil_Type12','Soil_Type13','Soil_Type14','Soil_Type15','Soil_Type16','Soil_Type17','Soil_Type18','Soil_Type19','Soil_Type20','Soil_Type21','Soil_Type22','Soil_Type23','Soil_Type24','Soil_Type25','Soil_Type26','Soil_Type27','Soil_Type28','Soil_Type29','Soil_Type30','Soil_Type31','Soil_Type32','Soil_Type33','Soil_Type34','Soil_Type35','Soil_Type36','Soil_Type37','Soil_Type38','Soil_Type39','Soil_Type40']]
+		x_train = self.train_data[['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area1','Wilderness_Area2','Wilderness_Area3','Wilderness_Area4','Soil_Type1','Soil_Type2','Soil_Type3','Soil_Type4','Soil_Type5','Soil_Type6','Soil_Type7','Soil_Type8','Soil_Type9','Soil_Type10','Soil_Type11','Soil_Type12','Soil_Type13','Soil_Type14','Soil_Type15','Soil_Type16','Soil_Type17','Soil_Type18','Soil_Type19','Soil_Type20','Soil_Type21','Soil_Type22','Soil_Type23','Soil_Type24','Soil_Type25','Soil_Type26','Soil_Type27','Soil_Type28','Soil_Type29','Soil_Type30','Soil_Type31','Soil_Type32','Soil_Type33','Soil_Type34','Soil_Type35','Soil_Type36','Soil_Type37','Soil_Type38','Soil_Type39','Soil_Type40']]
+		model = self.model_type
+		#predicted_results = model.predict(x_test)
+		predicted_results = model.predict(x_train)
 		
 		return predicted_results
 
@@ -55,7 +75,54 @@ class ForestCover(object):
 		for i in xrange(0,len(self.predicted_results)):
 			if y_train[i] == self.predicted_results[i]:
 				count +=1
+		
 		return count
+
+	def svm_linearkernel(self,x_parameters,y_parameters):
+
+		""" Support vector mechine with linear kernel to fit our train data """
+
+		self.x_parameters = x_parameters
+		self.y_parameters = y_parameters
+
+		C = 1.0  # SVM regularization parameter
+		svc = svm.SVC(kernel='linear', C=C).fit(self.x_parameters, self.y_parameters)
+		
+		return svc
+
+	def svm_rbfkernel(self,x_parameters,y_parameters):
+
+		""" Support vector mechine with Radial basis function kernel to fit our train data """
+
+		slef.x_parameters = x_parameters
+		self.y_parameters = y_parameters
+
+		C = 1.0  # SVM regularization parameter
+		rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(self.x_parameters, self.y_parameters)
+		
+		return rbf_svc
+
+	def svm_polykernel(self,x_parameters,y_parameters):
+
+		""" Support vector mechine with poly kernel to fit our train data """
+
+		self.x_parameters = x_parameters
+		self.y_parameters = y_parameters
+
+		C = 1.0  # SVM regularization parameter
+		poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(self.x_parameters, self.y_parameters)
+
+		return poly_svc
+
+	def svm_linear(self,x_parameters,y_parameters):
+
+		""" Support vector mechine wiht linear svm model to fit our train data """
+
+		self.x_parameters = x_parameters
+		self.y_parameters = y_parameters
+
+		C = 1.0  # SVM regularization parameter
+		lin_svc = svm.LinearSVC(C=C).fit(X, y)
 
 	def save_predicted_results(self,predicted_results):
 
@@ -74,11 +141,19 @@ def main():
 
 	train_csv = pd.read_csv('train.csv')
 	test_csv = pd.read_csv('test.csv')
-	print len(test_csv)
+	#print len(test_csv)
+	print len(train_csv)
 	forestcover = ForestCover(train_csv,test_csv)
-	results = forestcover.predict_test_data()
+	x_train,y_train = forestcover.convert_xy_traindata()
+	# svm_linearkernel_model = forestcover.svm_linearkernel(x_train,y_train)
+	# svm_rbfkernel_model = forestcover.svm_rbfkernel(x_train,y_train)
+	# svm_polykernel_model = forestcover.svm_polykernel(x_train,y_train)
+	# svm_linear_model = forestcover.svm_linear(x_train,y_train)
+	randomforest_model = forestcover.randomforest_classifier_model(x_train,y_train)
+	results = forestcover.predict_test_data(randomforest_model)
 	print len(results)
-	forestcover.save_predicted_results(results)
+	#print forestcover.check_predicted_model(results)
+	#forestcover.save_predicted_results(results)
 	#print forestcover.train_data
 if __name__ == "__main__":
 	main()
